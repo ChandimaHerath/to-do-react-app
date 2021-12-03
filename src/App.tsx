010cd32e731eb7f1795ab9c3593b2e4dfa6f1451
ChandimaHerath <chandimaherath@hotmail.com>
2021-12-03 20:55:39 +0530

    Form component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container bg-white p-4 mt-5'>
+     <h1>TO DO</h1>
+     <Form/>
     </div>
   );
 }
