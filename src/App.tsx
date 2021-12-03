e8784b91b4a258edf5f5cdc05f4c102099cef09b
ChandimaHerath <chandimaherath@hotmail.com>
2021-12-03 22:33:52 +0530

    todo list and todoitem components implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import Form from './components/Form';
+import TodoList from './components/Todolist';
 
 function App() {
   return (
     <div className='container bg-white p-4 mt-5'>
      <h1>TO DO</h1>
      <Form/>
+     <TodoList/>
     </div>
   );
 }
