3b943825246d6c87f4993cd612fd60b0aafa7257
ChandimaHerath <chandimaherath@hotmail.com>
2021-12-03 23:47:17 +0530

    Expired Todo conditional rendering implemented

--- src/components/Todoitem.tsx
+++ src/components/Todoitem.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const TodoItem = ( { id,title,completed}:any ) => {
+const TodoItem = ( { id,title,completed,expireDate}:any ) => {
 	return (
-		<li key={id} className={`list-group-item ${completed && 'list-group-item-success'}`}>
+		<li key={id} className={`list-group-item ${expireDate<= Date.now() && 'list-group-item-danger'}`}>
 			<div className='d-flex justify-content-between'>
 				<span className='d-flex align-items-center'>
 					<input type='checkbox' className='mr-3' checked={completed}></input>
 					{title}
 				</span>
 				<button className='btn btn-danger'>Delete</button>
-			</div>
+			</div> 
 		</li>
 	);
 };
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import TodoItem from './Todoitem';
+import TodoItem from './Todoitem'; 
 
 const TodoList = () => {
 	const todos = [
-		{ id: 1, title: 'todo1', completed: false},
-        { id: 2, title: 'todo2', completed: false },
-		{ id: 3, title: 'todo3', completed: true },
-		{ id: 4, title: 'todo4', completed: false },
-		{ id: 5, title: 'todo5', completed: false },
+		{ id: 1, title: 'todo1', completed: false, expireDate:1638554916470 },
+        { id: 2, title: 'todo2', completed: false,  expireDate:1638554916470 },
+		{ id: 3, title: 'todo3', completed: true ,  expireDate:1638554916470},
+		{ id: 4, title: 'todo4', completed: false,  expireDate:1638554916470 },
+		{ id: 5, title: 'todo5', completed: false,  expireDate:1699999999999 },
 	];
 
 	return (
 		<ul className='list-group'>
 			{todos.map((todo) => (
-				<TodoItem id={todo.id} title={todo.title} completed={todo.completed} />
+				<TodoItem id={todo.id} title={todo.title} completed={todo.completed} expireDate={todo.expireDate} />
 			))}
 		</ul>
 	);
